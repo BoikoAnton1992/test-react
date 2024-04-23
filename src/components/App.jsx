ebb86b87380e853ff3d83b297ef5895197ed1fb7
BoikoAnton1992 <145497164+BoikoAnton1992@users.noreply.github.com>
2024-04-23 18:25:21 +0300

    Test Route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,47 @@
-// src/components/App.jsx
+import { Routes, Route, NavLink } from 'react-router-dom';
+import clsx from 'clsx';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Products from '../pages/Ppprodacts';
+import ProductDetails from '../pages/Ppprodacts';
+import NotFound from '../pages/NotFound';
+import css from './App.module.css';
 
-import { Product } from "./Product";
+const buildLinkClass = ({ isActive }) => {
+  return clsx(css.link, isActive && css.active);
+};
 
-
-export default function App() {
+export const App = () => {
   return (
-    <div>
-      <h1>Best selling</h1>
+    <div className={css.container}>
+      <header className={css.header}>
+        <p className={css.logo}>
+          <span role="img" aria-label="computer icon">
+            💻
+          </span>{' '}
+          GoMerch Store
+        </p>
 
-      <Product
-        name="Tacos With Lime"
-        price={10.99}
-        // imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-      />
-      <Product
-        name="Fries and Burger"
-        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
-        price={14.29}
-      />
+        <nav className={css.nav}>
+          <NavLink to="/" className={buildLinkClass}>
+            Home
+          </NavLink>
+          <NavLink to="/about" className={buildLinkClass}>
+            About
+          </NavLink>
+          <NavLink to="/products" className={buildLinkClass}>
+            Products
+          </NavLink>
+        </nav>
+      </header>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetails />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
-}
+};
